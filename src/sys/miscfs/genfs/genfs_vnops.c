a53fa214b7395b3f635489a4a6ec62722f9718d8
phoenix synkova <synkova.work@gmail.com>
2021-12-01 12:48:38 -0800

    Update genfs_vnops.c

--- src/sys/miscfs/genfs/genfs_vnops.c
+++ src/sys/miscfs/genfs/genfs_vnops.c
@@ -1534,7 +1534,7 @@ genfs_create(void *v)
     int unlock = 1;
     int error = 0;
     //void *buf;
-    daddr_t blk;
+    //daddr_t blk;
     size_t dirsize = -1;
     size_t max_namesize = -1;
     MOP_GET_MAX_NAMESIZE(&max_namesize);
